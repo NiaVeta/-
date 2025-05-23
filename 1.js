ac1ce924d38d89b5d371d1ca47a79cf2550623dc
NiaVeta <168092767+NiaVeta@users.noreply.github.com>
2025-05-23 07:36:11 +0700

    1.js

--- 1.js
+++ 1.js
@@ -9,15 +9,12 @@ function encoder() {
         let index = alphabet.indexOf(char);
         
         if (index >= 0) {
-            // Lowercase letter
             result += alphabet[(index + 13) % 26];
         } else {
             index = alphabetTopCase.indexOf(char);
             if (index >= 0) {
-                // Uppercase letter
                 result += alphabetTopCase[(index + 13) % 26];
             } else {
-                // Not an alphabet letter - keep as is
                 result += char;
             }
         }
